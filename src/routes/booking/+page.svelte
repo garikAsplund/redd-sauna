4ba5cea7f89b7e70dfa15162720e376b5bfb410e
Garik Asplund <garik.asplund@gmail.com>
2024-11-03 22:42:45 -0800

    mobile improvements

--- src/routes/booking/+page.svelte
+++ src/routes/booking/+page.svelte
@@ -6,7 +6,7 @@
 	<title>Redd Sauna | Booking</title>
 </svelte:head>
 
-<section class="w-full px-8 pt-64 flex justify-center">
+<section class="w-full px-8 md:pt-64 pt-24 flex justify-center">
 	<iframe
 		scrolling="no"
 		src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;showTz=0&amp;showCalendars=0&amp;showPrint=0&amp;showTabs=0&amp;showTitle=0&amp;src=YTZsbWF2N2NzZzFxbGxwbWE0dHBoOWlhcnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D81B60&amp;wmode=opaque"
