d98b88bec98e894fddd57e2b7e972bc1d404cd82
Garik Asplund <garik.asplund@gmail.com>
2024-11-04 20:24:13 -0800

    better home button thx insta

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <nav
-	class="fixed top-0 z-40 w-full md:pb-20 font-bold {$page.url.pathname === '/'
+	class="fixed top-0 z-40 w-full font-bold md:pb-20 {$page.url.pathname === '/'
 		? 'text-white'
 		: 'bg-white text-black'}"
 	id="nav"
@@ -38,7 +38,7 @@
 		<a
 			href="/"
 			aria-label="Home"
-			class="absolute left-1/2 -translate-x-1/2 translate-y-1/6 transform hover:opacity-75 hidden md:block mt-4"
+			class="translate-y-1/6 absolute left-1/2 mt-4 hidden -translate-x-1/2 transform hover:opacity-75 md:block"
 			aria-current={$page.url.pathname === '/' ? 'page' : undefined}
 		>
 			<Logo />
@@ -46,25 +46,42 @@
 		<div class="flex items-center">
 			<a
 				href="/booking"
-				class="mx-4 p-2 px-4 text-2xl hover:opacity-75 hidden md:block {scrollY < 10 && $page.url.pathname === '/' ? 'invisible' : ''} {$page.url.pathname === '/booking'
+				class="mx-4 hidden p-2 px-4 text-2xl hover:opacity-75 md:block {scrollY < 10 &&
+				$page.url.pathname === '/'
+					? 'invisible'
+					: ''} {$page.url.pathname === '/booking'
 					? 'border-b-2 border-black'
 					: $page.url.pathname === '/'
-					? 'border-2 border-white'
-					: 'border-2 border-[#d33e27]'}"
+						? 'border-2 border-white'
+						: 'border-2 border-[#d33e27]'}"
 				style={$page.url.pathname === '/' ? `opacity: ${scrollY / 300}` : ``}
 				aria-label="Book now"
 				aria-current={$page.url.pathname === '/booking' ? 'page' : undefined}
 			>
 				Book Now
 			</a>
-			<a 
-				href="/" 
-				aria-label="Home" 
-				class="block md:hidden p-4 scale-125"
+			<a
+				href="/"
+				aria-label="Home"
+				class="block scale-125 p-4 md:hidden"
 				style={$page.url.pathname === '/' ? `opacity: ${scrollY / 300}` : ``}
 				aria-current={$page.url.pathname === '/' ? 'page' : undefined}
 			>
-			<svg aria-label="Home" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="M9.005 16.545a2.997 2.997 0 0 1 2.997-2.997A2.997 2.997 0 0 1 15 16.545V22h7V11.543L12 2 2 11.543V22h7.005Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
+				<svg
+					aria-label="Home"
+					fill="currentColor"
+					height="24"
+					role="img"
+					viewBox="0 0 24 24"
+					width="24"
+					><title>Home</title><path
+						d="M9.005 16.545a2.997 2.997 0 0 1 2.997-2.997A2.997 2.997 0 0 1 15 16.545V22h7V11.543L12 2 2 11.543V22h7.005Z"
+						fill="none"
+						stroke="currentColor"
+						stroke-linejoin="round"
+						stroke-width="2"
+					></path></svg
+				>
 			</a>
 			<Instagram />
 		</div>
