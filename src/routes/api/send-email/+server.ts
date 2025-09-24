621b07d7217fe92ef4b17249e78edb56019a0d6d
Garik Asplund <garik.asplund@gmail.com>
2025-09-24 16:36:21 -0700

    remove venmo link from email

--- src/routes/api/send-email/+server.ts
+++ src/routes/api/send-email/+server.ts
@@ -1,39 +1,39 @@
 import nodemailer from 'nodemailer';
 
 export async function POST({ request }) {
-    const formData = await request.json(); 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: import.meta.env.VITE_EMAIL_USER,
-            pass: import.meta.env.VITE_EMAIL_PASSWORD
-        }
-    });
+	const formData = await request.json();
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 587,
+		secure: false,
+		auth: {
+			user: import.meta.env.VITE_EMAIL_USER,
+			pass: import.meta.env.VITE_EMAIL_PASSWORD
+		}
+	});
 
-    const {
-        firstName,
-        lastName,
-        // preferredContactMethod,
-        email,
-        phoneNumber,
-        deliveryDate,
-        deliveryAddress,
-        additionalComments,
-        isGift,
-        numberOfDays,
-        subscribe
-    } = formData.data;
+	const {
+		firstName,
+		lastName,
+		// preferredContactMethod,
+		email,
+		phoneNumber,
+		deliveryDate,
+		deliveryAddress,
+		additionalComments,
+		isGift,
+		numberOfDays,
+		subscribe
+	} = formData.data;
 
-    const totalCost = numberOfDays * (numberOfDays > 1 ? 75 : 90);
+	const totalCost = numberOfDays * (numberOfDays > 1 ? 75 : 90);
 
-    // Email to the business
-    const businessMailOptions = {
-        from: 'reddsauna@gmail.com',
-        to: 'reddsauna@gmail.com', // reddsauna@gmail.com
-        subject: 'New Booking Request',
-        html: `
+	// Email to the business
+	const businessMailOptions = {
+		from: 'reddsauna@gmail.com',
+		to: 'reddsauna@gmail.com', // reddsauna@gmail.com
+		subject: 'New Booking Request',
+		html: `
             <h2>New Booking Request</h2>
             <p><strong>Name:</strong> ${firstName} ${lastName}</p>
             <p><strong>Email:</strong> ${email}</p>
@@ -48,14 +48,14 @@ export async function POST({ request }) {
             <p><strong>Gift:</strong> ${isGift ? 'Yes' : 'No'}</p>
             <p><strong>Additional Comments:</strong> ${additionalComments || 'None'}</p>
         `
-    };
+	};
 
-    // Email to the customer
-    const customerMailOptions = {
-        from: 'reddsauna@gmail.com',
-        to: email, // email
-        subject: 'Your Redd Sauna Booking Confirmation',
-        html: `
+	// Email to the customer
+	const customerMailOptions = {
+		from: 'reddsauna@gmail.com',
+		to: email, // email
+		subject: 'Your Redd Sauna Booking Confirmation',
+		html: `
             <h2>Thank you for booking with us, ${firstName}!</h2>
             <p>We've received your booking request:</p>
 			<div style="margin-left: 20px;">
@@ -67,20 +67,21 @@ export async function POST({ request }) {
                 ${deliveryAddress.city}, ${deliveryAddress.state} ${deliveryAddress.zipCode}</p>
             </div>
             ${isGift ? '<p><strong>Gift:</strong> Yes</p>' : ''}
-            ${additionalComments ? `<p><strong>Additional Comments:</strong> ${additionalComments}</p>`: ''}
+            ${additionalComments ? `<p><strong>Additional Comments:</strong> ${additionalComments}</p>` : ''}
 			</div>
-<p>The total cost for your booking is <strong>$${totalCost.toFixed(2)}</strong>. Please make your payment via Venmo to <a href="https://venmo.com/reddsauna" target="_blank">@reddsauna</a>.</p>            <p>We appreciate your business and look forward to serving you. If you have any further questions, please don't hesitate to reach out by replying to this email or contacting me at (541) 626-6261.</p>
-            <p>See you soon,<br>
+<p>The total cost for your booking is <strong>$${totalCost.toFixed(2)}</strong>.</p>            
+<p>We appreciate your business and look forward to serving you. If you have any further questions, please don't hesitate to reach out by replying to this email or contacting me at (541) 626-6261.</p>
+            <p>Warm regards,<br>
             Pip</p>
         `
-    };
+	};
 
-    try {
-        await transporter.sendMail(businessMailOptions);
-        await transporter.sendMail(customerMailOptions);
-        return new Response(JSON.stringify({ success: true }), { status: 200 });
-    } catch (error) {
-        console.error('Error sending email:', error);
-        return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
-    }
+	try {
+		await transporter.sendMail(businessMailOptions);
+		await transporter.sendMail(customerMailOptions);
+		return new Response(JSON.stringify({ success: true }), { status: 200 });
+	} catch (error) {
+		console.error('Error sending email:', error);
+		return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
+	}
 }
